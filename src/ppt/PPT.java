a5bc5439a8fb4343aa1ea6626be3143355e6193a
Emman <Emman@Emmanuel.bwrouter>
2024-02-03 12:37:32 -0600

    Version 1.0.0 de PPT

---
+++ src/ppt/PPT.java
@@ -0,0 +1,12 @@
+package ppt;
+
+import java.awt.Color;
+
+public class PPT {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.decode("0xcaf393"));
+    }
+    
+}
